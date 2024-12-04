73ecdbc2fe398ea9af1341fb39bdfdf9873782b9
iqooneo6 <sachinvel59@gmail.com>
2024-12-04 17:07:04 +0530

    1

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 8010,  // Set Vite to run on port 8010
-    proxy: {
+  // plugins: [react()],
+  // server: {
+  //   port: 8010,  // Set Vite to run on port 8010
+  //   proxy: {
+    export default defineConfig({
+      server: {
+        port: 8010,  // Vite will listen on port 8010
+        host: '0.0.0.0',  // Allow external connections
+      },
+    });
+    
       // When the request starts with /api, it will be proxied to the target server
       '/api': {
         target: 'http://65.0.93.147:4050', // The target backend server
